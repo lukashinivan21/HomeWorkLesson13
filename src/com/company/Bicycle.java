86a20f800e6e0348dfad1ed56afeb05945478365
Ivan <uvanes21@yandex.ru>
2022-02-15 08:49:43 +0300

    Домашнее задание готово для проверки

---
+++ src/com/company/Bicycle.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Bicycle extends Transport {
+
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Car car1 = new Car("Audi", 4);
+        Car car2 = new Car("BMW", 4);
+
+        Bicycle bicycle1 = new Bicycle("Аист", 2);
+        Bicycle bicycle2 = new Bicycle("Cube", 2);
+
+        Truck truck1 = new Truck("Камаз", 6);
+        Truck truck2 = new Truck("MAN", 8);
+
+        ServiceStation station = new ServiceStation();
+
+        station.check(bicycle1);
+        System.out.println();
+        station.check(bicycle2);
+        System.out.println();
+        station.check(car1);
+        System.out.println();
+        station.check(car2);
+        System.out.println();
+        station.check(truck1);
+        System.out.println();
+        station.check(truck2);
     }
 }
